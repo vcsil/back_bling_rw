88792a6da7dcd80e9edd5eeb403024c290777331
vcsil <vcsilva@poli.ufrj.br>
2024-03-14 18:29:38 -0300

    :sparkles: feat: create middleware for schema validation

---
+++ src/middlewares/schemaValidationMiddleware.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from "express";
+import { ObjectSchema } from "joi";
+import { MyCustomError, wrongSchemaError } from "../utils/errorUtils";
+
+type ReqHTTPTypes = "headers" | "body";
+
+type ParameterHeaderTypes = "x-api-key" | "authorization";
+
+export default function validateSchema(schema: ObjectSchema, reqHTTP: ReqHTTPTypes, parameterHeader?: ParameterHeaderTypes) {
+    return async (req: Request, _res: Response, next: NextFunction) => {
+        let objectHTTP;
+
+        if (parameterHeader) {
+            objectHTTP = req[reqHTTP][parameterHeader || ""];
+        } else {
+            objectHTTP = req[reqHTTP];
+        }
+
+        const { error } = schema.validate(objectHTTP, { abortEarly: false });
+
+        if (error) {
+            const errorMessages = error.details.map((detail) => detail.message);
+            throw new MyCustomError(wrongSchemaError(errorMessages.join(", ")));
+        }
+
+        next();
+    };
+}
